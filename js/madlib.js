10accf88c3dad1984432b44e38f2c3bda3382095
Devin Blankenship <failure2fly@gmail.com>
2017-02-13 14:08:54 -0500

    varibles and story

---
+++ js/madlib.js
@@ -0,0 +1,24 @@
+var adjective1 ='hairy';
+var adjective2 ='wild';
+var noun1 ='bagel';
+var noun2 ='school';
+var pluranNoun1 ='bungie cords';
+var game ='Call of Duty';
+var pluralNoun2 ='pallets';
+var verbEndingInIng1 ='skydiving';
+var verbEndingInIng2 ='caressing';
+var pluralNoun3 ='meerkats';
+var sport ='water polo';
+var verbEndingInIng3 ='racing';
+var noun3 ='cruise ship';
+var plant ='marijuana leaf';
+var partOfBody ='elbow';
+var place ='California';
+var verbEndingInIng4 ='biking';
+var adjective3 ='careful';
+var number ='8';
+var pluralNoun4 ='range rovers';
+
+var story = `A vacation is when you take a trip to some ${adjective1} place with your ${adjective2} family. Usually you go to some place that is near a/an ${noun1} or up on a/an ${noun2}. A good vacation place is one where you can ride ${pluranNoun1}. I like to spend my time ${verbEndingInIng1} or ${verbEndingInIng2}. When parents go on vacation, they spend their time eating three ${pluralNoun2} a day, and fathers play ${sport}, and mothers sit around ${verbEndingInIng3}. Last summer, my little brother fell in a/an ${noun3} and got poison ${plant} all over his ${partOfBody}. My family is going to go to (the) ${place}, and will practice ${verbEndingInIng4}. Parents need vacations more than kids becasue parents are always very ${adjective3} and because they have to work ${number} hours every day all year making enough ${pluralNoun4} to pay for the vacation. `;
+
+console.log(story);
